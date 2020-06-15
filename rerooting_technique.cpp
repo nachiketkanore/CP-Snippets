7cabbaf832b4e713236b9e05e0ec4edec8e57041
Nachiket Kanore <44920607+nachiketkanore@users.noreply.github.com>
2020-06-15 16:03:14 +0530

    Create rerooting_technique.cpp

---
+++ rerooting_technique.cpp
@@ -0,0 +1,56 @@
+//Problem : https://codeforces.com/problemset/problem/1324/F
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sz(x) (int)(x.size())
+#define FOR(x,y,z) for(int x = y; x <= z; x++)
+#define int long long
+
+const int N = 2e5 + 2, inf = 1e18;
+
+int n, u, v, a[N], dp[N], best[N];
+vector<int> g[N];
+
+void dfs1(int u, int par){
+    dp[u] = a[u];
+    for(int child : g[u]) if(child != par){
+        dfs1(child, u);
+        if(dp[child] > 0)   // take only positive branches to maximize answer
+            dp[u] += dp[child];
+    }
+}
+
+void dfs2(int u, int par){
+    best[u] = dp[u];
+    for(int child : g[u]) if(child != par){
+        dp[u] -= max(0LL, dp[child]);
+        dp[child] += max(0LL, dp[u]);
+        dfs2(child, u);
+        dp[child] -= max(0LL, dp[u]);
+        dp[u] += max(0LL, dp[child]);
+    }
+}
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);  
+  
+    cin >> n;
+    FOR(i,1,n) {
+        cin >> a[i];
+        if(a[i] == 0)
+            a[i] = -1;
+    }
+    FOR(i,2,n){
+        cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+
+    dfs1(1,1);
+    dfs2(1,1);
+
+    FOR(i,1,n) 
+        cout << best[i] << ' ';
+}
