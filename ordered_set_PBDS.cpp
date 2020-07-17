c5a9ce96571eca35c7f05031c49dce6f3f4eb065
Nachiket Kanore <44920607+nachiketkanore@users.noreply.github.com>
2020-07-17 18:24:12 +0530

    Create ordered_set_PBDS.cpp

---
+++ ordered_set_PBDS.cpp
@@ -0,0 +1,22 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+//Use pair<int,int> to support multiset like features
+typedef tree<pair<int,int>, null_type, less<pair<int,int>>, rb_tree_tag, tree_order_statistics_node_update> oset;
+
+oset curr;
+
+/*
+1. Number of elements strictly greater than a[i]
+int cnt = sz(curr) - (int)curr.order_of_key({a[i]+1,0});
+
+2. Number of elements strictly less than a[i]
+int cnt = (int)curr.order_of_key({a[i],i});
+
+3. Use insert like this : curr.insert({a[id], id});
+
+4. Erasing : curr.erase({a[i], i});
+*/
+
+NOTE : long long doesn't work with this.
