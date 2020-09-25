70c721ee27979fb3d538db1b5098e5fa226892d6
Nachiket Kanore <44920607+nachiketkanore@users.noreply.github.com>
2020-09-25 23:07:10 +0530

    Update gcc_pragma.cpp

--- gcc_pragma.cpp
+++ gcc_pragma.cpp
@@ -1,4 +1,7 @@
-
-#pragma GCC target ("avx2")
+#pragma comment(linker, "/stack:2000000000")  // 
 #pragma GCC optimization ("O3")
 #pragma GCC optimization ("unroll-loops")
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
