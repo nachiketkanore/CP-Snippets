c67dfa0cc99e4f267b0679610995f4aa4ed8ac2e
Nachiket Kanore <44920607+nachiketkanore@users.noreply.github.com>
2020-09-27 22:10:41 +0530

    Update gcc_pragma.cpp

--- gcc_pragma.cpp
+++ gcc_pragma.cpp
@@ -5,3 +5,9 @@
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
 #pragma GCC optimize("Ofast")
 #pragma GCC target("avx,avx2,fma")
+
+
+// For heavy recursions
+// #pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math,inline")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,lzcnt,mmx,abm,avx,avx2")
