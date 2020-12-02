5c2fde64e0cdb8864b39b521481ad561adcb12a6
Nachiket Kanore <44920607+nachiketkanore@users.noreply.github.com>
2020-12-02 23:06:59 +0530

    Create segtree_basic.cc

---
+++ segtree/basic_segtree/segtree_basic.cc
@@ -0,0 +1,92 @@
+class node {
+public:
+	static const int inf = 1e18;
+	int mn, mx, sum;
+
+	node(int _mn, int _mx, int _sum) {
+		mn = _mn;
+		mx = _mx;
+		sum = _sum;
+	}
+
+	node() {
+		mn = inf;
+		mx = -inf;
+		sum = 0;
+	}
+};
+
+// Default tasks
+// Point assignment with range queries
+struct segtree {
+	int N;
+	vector<node> st;
+	void init(int n) {
+		N = n;
+		st.resize((N<<2)+2);
+		build(1,1,N);
+	}
+
+	//Write reqd merge functions
+	void merge(node &ret, node &left, node &right) {
+		ret.mn = min(left.mn, right.mn);
+		ret.mx = max(left.mx, right.mx);
+		ret.sum = left.sum + right.sum;
+	}
+
+	void build (int u, int L, int R) {
+		if(L == R) {
+			// Leaf value
+			st[u] = node(0,0,0);
+			return;
+		}
+		int M = (L + R)/2;
+		build(u*2, L, M);
+		build(u*2 + 1, M + 1, R);
+		merge(st[u], st[u*2], st[u*2+1]);
+	}
+
+	node Query(int u, int L, int R, int i, int j) {
+		if(j<L || i>R)
+			return node();
+		if(i<=L && R<=j)
+			return st[u];
+		int M = (L + R)/2;
+		node left = Query(u*2, L, M, i, j);
+		node right = Query(u*2 + 1, M + 1, R, i, j);
+		node ret;
+		merge(ret, left, right);
+		return ret;
+	}
+
+	node pQuery(int u, int L, int R, int pos) {
+		if(L == R)
+			return st[u];
+		int M = (L + R)/2;
+		if(pos <= M)
+			return pQuery(u*2, L, M, pos);
+		else
+			return pQuery(u*2 + 1, M + 1, R, pos);
+	}	
+
+	void pUpdate(int u, int L, int R, int pos, int val) {
+		if(L == R) {
+			// Point update
+			// Update leaf value
+			st[u] = node(val, val, val);
+			return;
+		}
+		int M = (L + R)/2;
+		if(pos <= M)
+			pUpdate(u*2, L, M, pos, val);
+		else
+			pUpdate(u*2 + 1, M + 1, R, pos, val);
+		merge(st[u], st[u*2], st[u*2 + 1]);
+	}
+
+	node query(int pos) { return pQuery(1, 1, N, pos); }
+
+	node query(int l, int r) { return Query(1, 1, N, l, r); }
+
+	void update(int pos, int val) { pUpdate(1, 1, N, pos, val); }
+};
