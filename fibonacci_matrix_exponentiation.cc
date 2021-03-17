f430192d1576a453f7d6760c2625765e6cdc7897
Nachiket Kanore <44920607+nachiketkanore@users.noreply.github.com>
2021-03-17 12:12:20 +0530

    Create fibonacci_matrix_exponentiation.cc

---
+++ fibonacci_matrix_exponentiation.cc
@@ -0,0 +1,94 @@
+const int MOD = 1e9 + 7;
+int add(int a, int b)
+{
+	int res = a + b;
+	if(res >= MOD)
+		return res - MOD;
+	return res;
+}
+
+int mult(int a, int b)
+{
+	long long res = a;
+	res *= b;
+	if(res >= MOD)
+		return res % MOD;
+	return res;
+}
+
+const int SZ = 2;
+struct matrix
+{
+	int arr[SZ][SZ];
+
+	void reset()
+	{
+		memset(arr, 0, sizeof(arr));
+	}
+
+	void makeiden()
+	{
+		reset();
+		for(int i=0;i<SZ;i++)
+		{
+			arr[i][i] = 1;
+		}
+	}
+
+	matrix operator + (const matrix &o) const 
+	{
+		matrix res;
+		for(int i=0;i<SZ;i++)
+		{
+			for(int j=0;j<SZ;j++)
+			{
+				res.arr[i][j] = add(arr[i][j], o.arr[i][j]);
+			}
+		}
+		return res;
+	}
+
+	matrix operator * (const matrix &o) const 
+	{
+		matrix res;
+		for(int i=0;i<SZ;i++)
+		{
+			for(int j=0;j<SZ;j++)
+			{
+				res.arr[i][j] = 0;
+				for(int k=0;k<SZ;k++)
+				{
+					res.arr[i][j] = add(res.arr[i][j] , mult(arr[i][k] , o.arr[k][j]));
+				}
+			}
+		}
+		return res;
+	}
+};
+
+matrix power(matrix a, int b)
+{
+	matrix res;
+	res.makeiden();
+	while(b)
+	{
+		if(b & 1)
+		{
+			res = res * a;
+		}
+		a = a * a;
+		b >>= 1;
+	}
+	return res;
+}
+
+// fib(1) = fib(2) = 1
+int fib(int n) {
+	if (n == 1) return 1;
+	if (n == 2) return 1;
+	matrix X;
+	X.arr[0][0] = X.arr[0][1] = X.arr[1][0] = 1;
+	X.arr[1][1] = 0;
+	X = power(X, n-2);
+	return add(X.arr[0][0], X.arr[1][0]);
+}
