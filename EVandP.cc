385175c24a62d4ba350666774634963fe587a110
Nachiket Kanore <44920607+nachiketkanore@users.noreply.github.com>
2021-04-19 01:19:51 +0530

    Create EVandP.cc

---
+++ EVandP.cc
@@ -0,0 +1,10 @@
+# EV and probability problems
+
+amazing EV property, infinite sums in EV
+https://codedrills.io/contests/amrita-icpc-practice-session-6/problems/bob-and-random-exploration
+https://codedrills.io/submissions/47270
+
+polynomials idea, power of polynomial
+https://www.codechef.com/problems/EXPTPROD
+https://www.codechef.com/viewsolution/45136112
+
