a517de4cba822b22f29d31105bec577d919134a7
Nachiket Kanore <44920607+nachiketkanore@users.noreply.github.com>
2021-04-19 01:21:31 +0530

    Update EVandP.cc

--- EVandP.cc
+++ EVandP.cc
@@ -1,5 +1,14 @@
 # EV and probability problems
 
+# Good resources with examples to understand
+https://math.stackexchange.com/questions/1196452/expected-value-of-the-number-of-flips-until-the-first-head?noredirect=1&lq=1
+https://math.stackexchange.com/questions/1225152/expected-number-of-rolls-on-a-die-until-each-face-has-appeared-at-least-twice?rq=1
+https://math.stackexchange.com/questions/757149/expected-number-of-rolls-when-repeatedly-rolling-an-n-sided-die?rq=1
+https://math.stackexchange.com/questions/2412375/suppose-we-roll-a-fair-6-sided-die-repeatedly-find-the-expected-number-of-rol?rq=1
+
+
+# Solved problems 
+
 amazing EV property, infinite sums in EV
 https://codedrills.io/contests/amrita-icpc-practice-session-6/problems/bob-and-random-exploration
 https://codedrills.io/submissions/47270
